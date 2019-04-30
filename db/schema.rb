84f733c8094663d2dcfc7ab6291fe55948c2654b
DavidWagner17 <david_gui2k6@hotmail.com>
2019-04-30 01:06:18 +0000

    Corrected table description to .text.

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_005822) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
